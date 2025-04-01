ca11f813d5dc3143f435b42fefcb6ae840fd17ba
ithubtrtr <0xffd8ee@gmail.com>
2025-04-01 23:22:35 +0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,28 +1,27 @@
-import axios from 'axios';
+import fetch from 'node-fetch';
 
 export default async function handler(req, res) {
   try {
-    const url = 'https://oha.to/addon.watched'; // HTTPS bağlantısı
-
+    const url = 'https://oha.to/addon.watched';
     const { method, headers } = req;
 
     // User-Agent'ı kaldırıyoruz
     const headersWithoutUserAgent = { ...headers };
     delete headersWithoutUserAgent['user-agent'];
 
-    // Axios ile OHA.TO'ya yönlendirme
-    const response = await axios({
+    const response = await fetch(url, {
       method: method,
-      url: url,
       headers: headersWithoutUserAgent,
-      httpsAgent: new (require('https').Agent)({
-        rejectUnauthorized: false, // Sertifika hatalarını göz ardı etmek
-      }),
     });
 
-    res.status(response.status).send(response.data); // Yanıtı gönder
+    if (!response.ok) {
+      throw new Error(`Error: ${response.status} - ${response.statusText}`);
+    }
+
+    const data = await response.json(); // JSON yanıtı alıyoruz
+    res.status(response.status).json(data); // Yanıtı JSON olarak döndürüyoruz
   } catch (error) {
     console.error('Error in function:', error);
-    res.status(500).send({ error: 'Internal Server Error' });
+    res.status(500).send({ error: 'Internal Server Error', details: error.message });
   }
 }
