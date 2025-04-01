10c96b5df0a43d84288c7a7ebad5127b1e1c21a9
ithubtrtr <0xffd8ee@gmail.com>
2025-04-01 23:18:14 +0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,22 +1,30 @@
 import fetch from 'node-fetch';
 
 export default async function handler(req, res) {
-  const url = 'https://oha.to/addon.watched'; // OHA.TO API endpoint
-  const { method, headers } = req;
+  try {
+    const url = 'http://oha.to/addon.watched'; // HTTPS yerine HTTP'yi kullan
+    const { method, headers } = req;
 
-  // Proxy'den gelen istekte User-Agent'ı kaldırıyoruz
-  const headersWithoutUserAgent = { ...headers };
-  delete headersWithoutUserAgent['user-agent'];
+    // Proxy'den gelen istekte User-Agent'ı kaldırıyoruz
+    const headersWithoutUserAgent = { ...headers };
+    delete headersWithoutUserAgent['user-agent'];
 
-  // OHA.TO'ya gelen isteği yönlendiriyoruz
-  const response = await fetch(url, {
-    method: method,
-    headers: headersWithoutUserAgent,
-  });
+    // OHA.TO'ya gelen isteği yönlendiriyoruz
+    const response = await fetch(url, {
+      method: method,
+      headers: headersWithoutUserAgent,
+    });
 
-  // OHA.TO'dan gelen yanıtı proxy'ye iletilmesi için tekrar dönüyoruz
-  const data = await response.text(); // OHA.TO'dan gelen yanıtı alıyoruz
+    if (!response.ok) {
+      throw new Error('Error with OHA.TO response');
+    }
 
-  // OHA.TO'dan alınan yanıtı kullanıcıya gönderiyoruz
-  res.status(response.status).send(data);
+    const data = await response.text(); // OHA.TO'dan gelen yanıtı alıyoruz
+
+    // OHA.TO'dan alınan yanıtı kullanıcıya gönderiyoruz
+    res.status(response.status).send(data);
+  } catch (error) {
+    console.error('Error in function:', error);
+    res.status(500).send({ error: 'Internal Server Error' });
+  }
 }
