7f340e7a250e88ba3e7793973e99ea04afcf1e9a
ithubtrtr <0xffd8ee@gmail.com>
2025-04-01 22:35:45 +0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,30 +1,31 @@
-import { createRequire } from "module";
-const require = createRequire(import.meta.url);
 import fetch from "node-fetch";
 
 export default async function handler(req, res) {
-    // Kullanıcının WATCHED olup olmadığını kontrol et
-    let userAgent = req.headers["user-agent"] || "";
+    // WATCHED uygulamasından gelen User-Agent'i kaldır
+    const headers = {
+        "Host": "oha.to",
+        "Connection": "Keep-Alive",
+        "Accept-Encoding": "gzip, deflate"
+    };
 
-    // Hedef URL
+    // Hedef URL (oha.to)
     const targetUrl = "https://oha.to/addon.watched";
 
-    // Eğer WATCHED kullanıcısıysa User-Agent kaldır ve isteği ilet
-    const response = await fetch(targetUrl, {
-        method: "GET",
-        headers: {
-            "Host": "oha.to",
-            "Connection": "Keep-Alive",
-            "Accept-Encoding": "gzip, deflate"
-        },
-    });
+    try {
+        const response = await fetch(targetUrl, {
+            method: "GET",
+            headers: headers
+        });
 
-    // Gelen yanıtı uygun şekilde döndür
-    res.status(response.status);
-    response.headers.forEach((value, key) => {
-        res.setHeader(key, value);
-    });
-
-    const data = await response.buffer();
-    res.send(data);
+        // Yanıt başlıklarını ayarla
+        res.setHeader("Content-Type", response.headers.get("Content-Type"));
+        res.setHeader("Access-Control-Allow-Origin", "*"); // CORS için
+        
+        // Durum kodunu ayarla ve yanıtı gönder
+        res.status(response.status);
+        const data = await response.text();
+        res.send(data);
+    } catch (error) {
+        res.status(500).send("Bağlantı hatası: " + error.message);
+    }
 }
