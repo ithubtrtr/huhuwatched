d02ae822d6e5d5c3558997f83bc7d064bb4d7e03
ithubtrtr <0xffd8ee@gmail.com>
2025-04-05 01:02:43 +0300

    Create proxy.js

---
+++ api/proxy.js
@@ -0,0 +1,15 @@
+export default async function handler(req, res) {
+  const response = await fetch("http://oha.to/addon.watched", {
+    method: "GET",
+    headers: {
+      // deliberately omit user-agent!
+      "Host": "oha.to",
+      "Connection": "Keep-Alive",
+      "If-Modified-Since": "Fri, 04 Apr 2025 21:39:01 GMT",
+      "Accept-Encoding": "gzip, deflate"
+    }
+  });
+
+  const data = await response.text(); // JSON veya düz metin olabilir
+  res.status(response.status).send(data);
+}
